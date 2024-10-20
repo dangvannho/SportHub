3785365346cfd5b6223b8617e78865babcf4a0f2
hieuminh17 <minhhieuthkcr4469@gmail.com>
2024-10-20 23:24:37 +0700

    Add EC, EM in authControllers.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     phone_number: { type: String, required: true, unique: true },
-    account_status: { type: String, enum: ['active', 'inactive', 'suspended'], default: 'active' },
-    user_role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    account_status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+    },
+    user_role: { type: String, enum: ["user", "admin"], default: "user" },
     profile_picture: { type: String },
     verificationToken: { type: String },
-    isVerified: { type: Boolean, default: false }
-}, { timestamps: true });
+    isVerified: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
