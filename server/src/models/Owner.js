9ada15d512b19d2fa9388e29fd58e60d6ae541c3
Hung <tranquochung4923@gmail.com>
2024-10-24 22:03:55 +0700

    update schema

--- server/src/models/Owner.js
+++ server/src/models/Owner.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const OwnerSchema = new mongoose.Schema({
     business_name: { type: String, required: true },
     address: { type: String, required: true },
-    phone_number: { type: String, required: true },
-    email: { type: String, required: true },
+    phone_number: { type: String, required: true, unique: true },
+    email: { type: String, required: true , unique: true },
     password: { type: String, required: true },
     profile_picture: { type: String },
     citizen_identification_card: { type: String },
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,15 +5,9 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone_number: { type: String, required: true, unique: true },
-    account_status: {
-      type: String,
-      enum: ["active", "inactive", "suspended"],
-      default: "active",
-    },
+    phone_number: { type: String, unique: true },
     user_role: { type: String, enum: ["user", "admin"], default: "user" },
     profile_picture: { type: String },
-    verificationToken: { type: String },
     isVerified: { type: Boolean, default: false },
   },
   { timestamps: true }
