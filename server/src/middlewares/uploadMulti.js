f1886179616c20f256f15d958dd47e0ea9cde1fe
lilducanh <ducanh8903@gmail.com>
2024-10-30 20:45:14 +0700

    Upload multi file

--- server/src/middlewares/uploadMulti.js
+++ server/src/middlewares/uploadMulti.js
@@ -1,11 +1,9 @@
-// uploadMiddleware.js
-
 const multer = require('multer');
 const path = require('path');
 
 // Cấu hình lưu trữ
 const storage = multer.memoryStorage(); // Lưu trữ ảnh trong bộ nhớ
-        
+
 const fileFilter = (req, file, cb) => {
     const allowedTypes = /jpeg|jpg|png/;
     const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
@@ -18,10 +16,10 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-const upload = multer({
+const upload_multi = multer({
     storage: storage,
     limits: { fileSize: 1024 * 1024 * 5 }, // Giới hạn kích thước file là 5MB
     fileFilter: fileFilter
 });
 
-module.exports = upload;
+module.exports = upload_multi.array('images', 10); // Cho phép tải lên tối đa 10 tệp ảnh với tên trường là 'images'
