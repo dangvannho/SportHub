9041dbd64633e7c902e6b42b966d3d844b1d0072
lilducanh <ducanh8903@gmail.com>
2024-11-25 23:05:18 +0700

    Update payment

--- server/src/index.js
+++ server/src/index.js
@@ -101,10 +101,7 @@ const middlewareController = require("../../server/src/controllers/middlewareCon
 const mongoose = require('mongoose');
 
 app.post('/payment', middlewareController.verifyToken, async (req, res) => {
-  const embed_data = {
-    redirecturl: "https://mydtu.duytan.edu.vn/Signin.aspx",
-    field_id: req.body._id, // Thêm _id vào embed_data
-  };
+  
 
   const items = [];
   const transID = Math.floor(Math.random() * 1000000);
@@ -128,6 +125,14 @@ app.post('/payment', middlewareController.verifyToken, async (req, res) => {
     console.error("Error fetching field availability:", error);
     return res.status(500).json({ EC: 0, EM: "Lỗi server" });
   }
+
+  id = availability.field_id;
+  console.log("Field ID:", id); 
+  
+  const embed_data = {
+    redirecturl: `http://localhost:5173/booking/${id}`,
+    field_id: req.body._id, // Thêm _id vào embed_data
+  };
 
   const availability_date = moment(availability.availability_date).format('DD-MM-YYYY');
   const Field_name = (await FieldAvailability.findById(_id)
